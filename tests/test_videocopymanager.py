97e14405e9ddabd69cb62afb1c551a1066fbbb02
Florian Zierler <zierler25@gmail.com>
2016-12-27 13:42:02 +0100

    Further test get_files_in_folder_recursive and start implementing get_video_files_in_folder_recursive

--- tests/test_videocopymanager.py
+++ tests/test_videocopymanager.py
@@ -9,52 +9,65 @@ class TestVideoCopyManager(unittest.TestCase):
     target = tempfile.TemporaryDirectory()
     vcm = VideoCopyManager(source.name,target.name)
 
-    def testVideoCopyManagerCreation(self):
+    def test_video_copy_manager_creation(self):
         """
         Make sure Video Copy Manager is imported
         """
         assert self.vcm is not None
 
-    def testBothFoldersExist(self):
+    def test_both_folders_exist(self):
         """
         Make sure the project is only run when both folders exist
         """
-        self.assertTrue(self.vcm.bothFoldersExist())
+        self.assertTrue(self.vcm.both_folders_exist())
 
         nullPath = "/dev/null/file"
         tmpFile = tempfile.TemporaryFile()
 
         vcm = VideoCopyManager(nullPath,self.target.name)
         with self.assertRaises(TypeError):
-            vcm.bothFoldersExist()
+            vcm.both_folders_exist()
 
         vcm = VideoCopyManager(self.source.name,nullPath)
         with self.assertRaises(TypeError):
-            vcm.bothFoldersExist()
+            vcm.both_folders_exist()
 
         vcm = VideoCopyManager(nullPath,nullPath)
         with self.assertRaises(TypeError):
-            vcm.bothFoldersExist()
+            vcm.both_folders_exist()
 
         vcm = VideoCopyManager(self.source.name,tmpFile.name)
         with self.assertRaises(TypeError):
-            vcm.bothFoldersExist()
+            vcm.both_folders_exist()
 
         vcm = VideoCopyManager(tmpFile.name,self.target.name)
         with self.assertRaises(TypeError):
-            vcm.bothFoldersExist()
+            vcm.both_folders_exist()
 
-    def testGetFilesInFolder(self):
+    def test_get_files_in_folder(self):
         """
         Assert 5 created files are found in TemporaryFolder
         :return:
         """
         tmp_dir = tempfile.TemporaryDirectory()
-        open(os.path.join(tmp_dir.name,"file"), 'wb')
-        os.mkdir(os.path.join(tmp_dir.name,"dir"))
-        open(os.path.join(tmp_dir.name,"dir","file"), 'wb')
-        open(os.path.join(tmp_dir.name,"dir","file1"), 'wb')
-        os.mkdir(os.path.join(tmp_dir.name,"dir","dir"))
-        open(os.path.join(tmp_dir.name,"dir","dir","file"), 'wb')
-        open(os.path.join(tmp_dir.name,"dir","dir","file1"), 'wb')
-        self.assertEqual(5,len(self.vcm.getFilesInFolder(tmp_dir.name)))
+        file1 = os.path.join(tmp_dir.name,"file")
+        dir1 = os.path.join(tmp_dir.name,"dir")
+        file2 = os.path.join(dir1,"file")
+        file3 = os.path.join(dir1,"file1")
+        dir2 = os.path.join(dir1,"dir")
+        file4 = os.path.join(dir2,"file")
+        file5 = os.path.join(dir2,"file1")
+        open(file1, 'wb')
+        os.mkdir(dir1)
+        open(file2, 'wb')
+        open(file3, 'wb')
+        os.mkdir(dir2)
+        open(file4, 'wb')
+        open(file5, 'wb')
+        result = self.vcm.get_files_in_folder_recursive(tmp_dir.name)
+        self.assertEqual(5, len(result))
+        self.assertTrue(result.__contains__(file1))
+        self.assertTrue(result.__contains__(file2))
+        self.assertTrue(result.__contains__(file3))
+        self.assertTrue(result.__contains__(file4))
+        self.assertTrue(result.__contains__(file5))
--- videocopymanager.py
+++ videocopymanager.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import os, glob, sys
+import os, magic
 
 class VideoCopyManager(object):
 
@@ -7,16 +7,27 @@ class VideoCopyManager(object):
         self.source = source
         self.target = target
 
-    def bothFoldersExist(self):
+    def both_folders_exist(self):
         if not os.path.isdir(self.source) or not os.path.isdir(self.target):
             raise TypeError('Either the provided source folder (' + self.source +
                             ') or the provided target folder (' + self.target +
                             ') does not exist in the file system.')
         return True
 
-    def getFilesInFolder(self,path):
+    def get_files_in_folder_recursive(self, path):
         files = []
         for root, subFolders, dirFiles in os.walk(path):
             for file in dirFiles:
+                filepath = os.path.join(root,file)
+                files.append(filepath)
+        return files
+
+    def get_video_files_in_folder_recursive(self,path):
+        files = []
+        for file in self.get_files_in_folder_recursive(path):
+            mime = magic.Magic(mime=True)
+            mimetype = mime.from_file(filename=file)
+            filetype = mimetype.split("/")[0]
+            if filetype is "video":
                 files.append(file)
         return files
